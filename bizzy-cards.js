f59c83b3b7aea7eba31686a06575ab70956478a2
Bryan Stewart <bryanrstewart@outlook.com>
2022-07-04 11:10:30 -0400

    lets go retry

--- bizzy-cards.js
+++ bizzy-cards.js
@@ -396,10 +396,12 @@ class FlowerCard extends LitElement {
 
     return html`
       <ha-card>
-        <div 
-          class="header clickable" @action=${(ev) => {
+        <div
+          class="header clickable"
+          @action=${(ev) => {
             console.log('I was clicked, or something', ev)
-          }}">
+          }}
+        >
           <img src="${stateObj.attributes.image}" />
           <span id="name"> ${stateObj.attributes.name} <ha-icon .icon="mdi:${stateObj.state == 'problem' ? 'alert-circle-outline' : ''}"></ha-icon></span>
           <span id="species">${species} </span>
