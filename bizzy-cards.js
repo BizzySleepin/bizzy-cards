4d129364b11c59924d54b7307cbacbd1d408748a
Bryan Stewart <bryanrstewart@outlook.com>
2022-07-04 21:08:04 -0400

    fixrd icon

--- bizzy-cards.js
+++ bizzy-cards.js
@@ -512,11 +512,11 @@ class leaksCard2 extends LitElement {
 
   render() {
     return html`
-    <ha-card>
-      <div class="shape">
-        <ha-icon icon="mdi:water'></ha-icon>
-      </div>
-    </ha-card>
+      <ha-card>
+        <div class="shape">
+          <ha-icon icon="mdi:water"></ha-icon>
+        </div>
+      </ha-card>
     `
   }
 
