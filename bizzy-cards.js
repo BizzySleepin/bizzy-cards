e2e246bf441c683c9cf0e205a695b380ab8c969b
Bryan Stewart <bryanrstewart@outlook.com>
2022-07-06 22:04:15 -0400

    yeeee

--- bizzy-cards.js
+++ bizzy-cards.js
@@ -709,8 +709,11 @@ class chipBoxCard extends LitElement {
   }
 
   render() {
+    const icon = (i) => {
+      return html`<ha-icon icon="mdi:${i}"></ha-icon>`
+    }
     const chip = () => {
-      return html`<div>🏠 2 / 0</div>`
+      return html`<div class="chip shape title">${icon(this.config.icon)}${this.config.text}${this.hass.states[this.config.entity]} / 0</div>`
     }
 
     return html` <ha-card> ${chip()} </ha-card> `
